bdbe0673145d35d64fcd6d8bbeea4d3faad29406
Brent Bolleman <bbolleman@gmail.com>
2025-02-11 13:23:04 -0800

    removed testtube emoji from home

--- src/taskpane/components/HomeTab.jsx
+++ src/taskpane/components/HomeTab.jsx
@@ -18,7 +18,7 @@ const HomeTab = ({ onTabClick, setGeneratedCode, setSelectedFunction, loadFuncti
     return (
         <>
             <div className="p-1 mb-32">
-                <h2 className="text-center text-lg font-semibold mb-2">Python functions in Excel 🧪</h2>
+                <h2 className="text-center text-lg font-semibold mb-2">Python functions in Excel</h2>
                 <div className="py-1 border-gray-300 rounded-lg p-2 mb-2">
                     <p><span className="font-bold">Step 1:</span> Write a Python function in the <span className="text-blue-500 underline cursor-pointer" onClick={() => onTabClick('editor')}>editor</span>.</p>
                     <div className="py-2 h-[75px]">
