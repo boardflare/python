49a45b3b5a7ea369c725b8d9e77620b20732b0f6
Brent Bolleman <bbolleman@gmail.com>
2025-02-26 11:54:26 -0800

    update messaging for OneDrive functions and refresh login button label

--- src/taskpane/components/SettingsTab.jsx
+++ src/taskpane/components/SettingsTab.jsx
@@ -47,7 +47,7 @@ const SettingsTab = ({ loadFunctions }) => {
                         className="px-2 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors font-normal"
                         disabled={isLoading}
                     >
-                        Update Token
+                        Refresh Login
                     </button>
                 </div>
                 <div className="">
