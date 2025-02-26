c1fba9c5495fa29b07de424d2e23c1d419632bce
Brent Bolleman <bbolleman@gmail.com>
2025-02-25 20:00:43 -0800

    change title

--- src/taskpane/components/AddFunctions.jsx
+++ src/taskpane/components/AddFunctions.jsx
@@ -51,7 +51,7 @@ const AddFunctions = ({ loadFunctions }) => {
     return (
         <div className="h-full flex flex-col">
             <div className="shrink-0 px-4 py-2 bg-gray-100 font-bold text-center">
-                Add Functions
+                Example Functions
             </div>
             <div className="flex-1 flex flex-col min-h-0">
                 <div className="flex-1 overflow-y-auto">
