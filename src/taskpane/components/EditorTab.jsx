63e4f9d0e0fad811f9e10e9c363643540531a764
Brent Bolleman <bbolleman@gmail.com>
2025-02-25 19:56:03 -0800

    notification for user to sign in to use onedrive.

--- src/taskpane/components/EditorTab.jsx
+++ src/taskpane/components/EditorTab.jsx
@@ -162,10 +162,9 @@ const EditorTab = ({
                 <div className="mt-2 p-2 bg-yellow-100 rounded">
                     <h2 className="font-semibold"> ⬅️ Drag task pane open for more room.</h2>
                     <ul className="list-disc pl-5">
-                        <li>Your code ⚠️MUST BE A FUNCTION!⚠️</li>
+                        <li>Your code MUST BE A FUNCTION!</li>
                         <li>Save will update code if function already exists.</li>
-                        <li>If signed in, function will also be saved to OneDrive.</li>
-                        <li>See <a href="https://whistlernetworks.sharepoint.com/:p:/s/Boardflare/EavKXzTcSmJArk1FadRoH40BaFTd1xrff2cw3bGSRs3AFg?rtime=Mhp28Ns33Ug" target="_blank" rel="noopener" className="text-blue-500 underline">slideshow</a> and <a href="https://www.boardflare.com/apps/excel/python/documentation" target="_blank" rel="noopener" className="text-blue-500 underline">documentation</a> for details.</li>
+                        <li>See <a href="https://www.boardflare.com/apps/excel/python/documentation" target="_blank" rel="noopener" className="text-blue-500 underline">documentation</a> for details.</li>
                     </ul>
                 </div>
             )}
--- src/taskpane/components/FunctionsTab.jsx
+++ src/taskpane/components/FunctionsTab.jsx
@@ -136,6 +136,20 @@ const FunctionsTab = ({
                     <FunctionTable functions={workbookFunctions} source="workbook" />
                 )}
 
+                {!isLoading && !folderUrl && (
+                    <div className="p-2 bg-blue-50 text-gray-800 rounded-lg mb-1">
+                        <div className="flex items-center gap-2 justify-center mb-2">
+                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <span className="font-semibold">OneDrive Functions</span>
+                        </div>
+                        <p className="">
+                            Add Files.ReadWrite permission in settings ⚙️ and login to save functions to OneDrive and use in other workbooks.
+                        </p>
+                    </div>
+                )}
+
                 {isLoading ? (
                     <div className="p-4 text-gray-900 text-center">
                         Loading OneDrive functions...
