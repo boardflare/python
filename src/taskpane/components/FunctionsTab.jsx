690709c87f270e2101b9702bfe9618e72d90fa5d
Brent Bolleman <bbolleman@gmail.com>
2025-03-02 17:25:41 -0800

    hide shared functions

--- src/taskpane/components/FunctionsTab.jsx
+++ src/taskpane/components/FunctionsTab.jsx
@@ -178,7 +178,7 @@ const FunctionsTab = ({
             </div>
 
             <Notebooks onImportComplete={() => loadFunctions()} />
-            <SharedFunctions onImportComplete={() => loadFunctions()} />
+            {/* <SharedFunctions onImportComplete={() => loadFunctions()} /> */}
 
             {deleteConfirm && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
