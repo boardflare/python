213a5724dcf9657057642a9c1fdb45b1230ef8a9
Brent Bolleman <bbolleman@gmail.com>
2025-05-02 21:28:16 -0700

    fix: update error message in execPython function and enhance output tab button with icon

--- src/functions/exec/controller.js
+++ src/functions/exec/controller.js
@@ -76,6 +76,6 @@ export async function execPython({ code, arg1 }, isName = true) {
             ConsoleEvents.emit(EventTypes.LOG, stdout.trim());
         }
         ConsoleEvents.emit(EventTypes.ERROR, message);
-        return [[`Error, also see Output tab: ${message} \n${stdout}`]];
+        return [[`Error, see ℹ️ tab for details: ${message} \n${stdout}`]];
     }
 }
